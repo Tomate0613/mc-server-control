c6a3d6cba11acee471fd7600192418856f87e3c1
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2022-05-20 18:06:16 +0200

    Add mods.css

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-await-in-loop */
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
@@ -13,6 +14,10 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const config = require('./config.json');
 const extract = require('extract-zip');
+const fetch = require('node-fetch');
+const crypto = require('crypto');
+
+const runningServers = new Map();
 
 //enable files upload
 app.use(
@@ -23,9 +28,8 @@ app.use(
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan('dev'));
-
-
+if (config.extensiveLogging)
+    app.use(morgan('dev'));
 app.use('/static', express.static('static'));
 app.set('view engine', 'ejs');
 
@@ -53,7 +57,7 @@ app.set('view engine', 'ejs');
     });
 
     //Server page
-    app.get('/servers/:id', async function (req, res) {
+    app.get('/servers/:id', async (req, res) => {
         if (!req.params.id || req.params.id === 'undefined') {
             return res.send({status: 'ERROR'});
         }
@@ -61,15 +65,61 @@ app.set('view engine', 'ejs');
         res.render('server.ejs', { server: result });
     });
 
+    //Mod page
+    app.get('/servers/:id/mods', async (req, res) => {
+        if (!req.params.id || req.params.id === 'undefined') {
+            return res.send({status: 'ERROR'});
+        }
+
+        const serverDir = `minecraft/servers/${req.params.id}`;
+        const result = await db.get('SELECT * FROM servers WHERE id = ?', [req.params.id]);
+
+        if (!fs.existsSync(`${serverDir}/mods`))
+            fs.mkdirSync(`${serverDir}/mods`);
+
+        const mods = fs.readdirSync(`${serverDir}/mods`);
+
+        let modList = [];
+        for (let i = 0; i < mods.length; i++) {
+            // eslint-disable-next-line camelcase
+            modList.push({title: mods[i], icon_url: '/static/icons/favicon.ico', description: 'Loading', body: '', file: mods[i]});
+        }
+
+        res.render('mods.ejs', { server: result, mods: modList });
+
+        modList = await getModList(serverDir);
+        if (modList)
+            io.emit('mod_list', modList);
+    });
+
+    //Plugin page
+    app.get('/servers/:id/plugins', async function (req, res) {
+        if (!req.params.id || req.params.id === 'undefined') {
+            return res.send({status: 'ERROR'});
+        }
+
+        const serverDir = `minecraft/servers/${req.params.id}`;
+        const result = await db.get('SELECT * FROM servers WHERE id = ?', [req.params.id]);
+
+        if (!fs.existsSync(`${serverDir}/plugins`))
+            fs.mkdirSync(`${serverDir}/plugins`);
+
+        const plugins = fs.readdirSync(`${serverDir}/plugins`);
+
+
+        res.render('plugins.ejs', { server: result, plugins });
+    });
+
     //Create server
     app.post('/api/server', async (req, res) => {
         const name = req.body.name;
         const software = req.body.software;
+        const version = req.body.version;
 
         if (!name || !software) return res.status(400).send({ status: 'ERROR', message: 'invalid form data' });
 
         // Create database record and server directory
-        const result = await db.run('INSERT INTO servers (name, status, software) VALUES (?, ?, ?)', [name, 'creating', software]);
+        const result = await db.run('INSERT INTO servers (name, status, software, version) VALUES (?, ?, ?, ?)', [name, 'creating', software, version]);
         serverListUpdate(await db.all('SELECT * FROM servers'));
         const serverID = result.lastID;
         const serverDir = `minecraft/servers/${serverID}`;
@@ -114,13 +164,14 @@ app.set('view engine', 'ejs');
         if (config.eula)
             fs.writeFileSync(`${serverDir}/eula.txt`, config.eulaText);
 
-
         let jar = `minecraft/software/${serverData.software.toLowerCase()}.jar`;
 
         if (!fs.existsSync(jar))
             return res.send({ status: 'ERROR', message: `Could not find ${jar}` });
         else
             console.log(`Starting server with jarfile ${jar}`);
+
+        let logs = '';
 
         const serverProcess = cp.spawn('java', [
             '-jar',
@@ -129,13 +180,13 @@ app.set('view engine', 'ejs');
         ], { cwd: serverDir });
 
         serverProcess.stdout.on('data', (data) => {
-            console.log(`stdout: ${data}`);
-            io.emit('server_logs', { server: serverID, data: data.toString() });
+            if (config.extensiveLogging)
+                console.log(`stdout: ${data}`.trimEnd());
+            logs += data.toString();
+            io.emit('server_logs', { server: serverID, data: logs });
         });
 
-        serverProcess.stderr.on('data', (data) => {
-            console.error(`stderr: ${data}`);
-        });
+        runningServers.set(serverDir, serverProcess);
 
         await db.run('UPDATE servers SET status = ? WHERE id = ?', ['running', serverID]);
         serverListUpdate(await db.all('SELECT * FROM servers'));
@@ -157,8 +208,19 @@ app.set('view engine', 'ejs');
     app.post('/api/server/:id/stop', async (req, res) => {
         const serverID = req.params.id;
         const serverDir = `minecraft/servers/${serverID}`;
-        const serverData = await db.get('SELECT * FROM servers WHERE id = ?', [serverID]);
-        res.send({ status: 'ERROR', message: 'Not implemented yet!' });
+        //const serverData = await db.get('SELECT * FROM servers WHERE id = ?', [serverID]);
+        runningServers.forEach(async (serverProcess, dir) => {
+            if (dir === serverDir) {
+                console.log('killing server process');
+                serverProcess.stdin.write('/stop\n');
+                sleep(5000).then(() => {
+                    //TODO: Force
+                    serverProcess.kill(0);
+                });
+            }
+        });
+
+        res.send({ status: 'OK' });
     });
 
     //Upload server zip
@@ -214,9 +276,106 @@ app.set('view engine', 'ejs');
     //Change server name
     app.post('/api/server/:id/change/name', async (req, res) => {
         const serverID = req.params.id;
-        let value = req.body.value;
+        const value = req.body.value;
 
         await db.run('UPDATE servers SET name = ? WHERE id = ?', [value, serverID]);
+
+        res.send({ status: 'OK' });
+    });
+
+    //Change server version
+    app.post('/api/server/:id/change/version', async (req, res) => {
+        const serverID = req.params.id;
+        const value = req.body.value;
+
+        await db.run('UPDATE servers SET version = ? WHERE id = ?', [value, serverID]);
+
+        res.send({ status: 'OK' });
+    });
+
+    //Install a mod
+    app.post('/api/server/:id/mods/:slug', async (req, res) => {
+        const serverID = req.params.id;
+        const slug = req.params.slug;
+        const serverDir = `minecraft/servers/${serverID}`;
+        const serverData = await db.get('SELECT * FROM servers WHERE id = ?', [serverID]);
+
+        if (!serverData) return res.send({ status: 'ERROR', message: 'Server not found' });
+        if (!fs.existsSync(serverDir)) return res.send({ status: 'ERROR', message: 'Server folder not found!' });
+
+        console.log(`Downloading mod ${slug}`);
+
+        // Get mod
+        try {
+            const response = await fetch(`https://api.modrinth.com/v2/project/${slug}/version`);
+            const mod = await response.json();
+
+            const index = mod.findIndex((version) => version.game_versions.includes(serverData.version) && version.loaders.includes(serverData.software.toLowerCase()));
+            const version = mod[index];
+
+            if (!version) return res.send({ status: 'ERROR', message: 'No compatible mod version found' });
+
+            // Get mod file
+            const modFile = version.files[0].url;
+            const modFilename = version.files[0].filename;
+            if (!fs.existsSync(`${serverDir}/mods`))
+                fs.mkdirSync(`${serverDir}/mods`);
+
+            console.log(`Downloading mod ${modFile.replace('cdn', 'cdn-raw')}`);
+
+            await downloadFile(modFile.replace('cdn', 'cdn-raw'), `minecraft/servers/${serverID}/mods/${modFilename}`);
+
+            const modList = await getModList(serverDir);
+
+            if (modList)
+                io.emit('mod_list', modList);
+
+            console.log(`Mod ${modFilename} downloaded`);
+            return res.send({ status: 'OK' });
+        } catch (e) {
+            return res.send({ status: 'ERROR', message: e.toString() });
+        }
+    });
+
+    //Delete a mod
+    app.delete('/api/server/:id/mods/:filename', async (req, res) => {
+        const serverID = req.params.id;
+        const filename = req.params.filename;
+        const serverDir = `minecraft/servers/${serverID}`;
+        const serverData = await db.get('SELECT * FROM servers WHERE id = ?', [serverID]);
+        if (!serverData) return res.send({ status: 'ERROR', message: 'Server not found' });
+        if (!fs.existsSync(serverDir)) return res.send({ status: 'ERROR', message: 'Server folder not found!' });
+
+        const file = `${serverDir}/mods/${filename}`;
+
+        if (!fs.existsSync(file))
+            return res.send({ status: 'ERROR', message: 'Mod not found' });
+
+        fs.unlinkSync(file);
+
+        const modList = await getModList(serverDir);
+
+        if (modList)
+            io.emit('mod_list', modList);
+
+        res.send({ status: 'OK' });
+    });
+
+    //Delete a plugin
+    app.delete('/api/server/:id/plugins/:filename', async (req, res) => {
+        const serverID = req.params.id;
+        const filename = req.params.filename;
+        const serverDir = `minecraft/servers/${serverID}`;
+        const serverData = await db.get('SELECT * FROM servers WHERE id = ?', [serverID]);
+        if (!serverData) return res.send({ status: 'ERROR', message: 'Server not found' });
+        if (!fs.existsSync(serverDir)) return res.send({ status: 'ERROR', message: 'Server folder not found!' });
+
+        const file = `${serverDir}/plugins/${filename}`;
+
+        if (!fs.existsSync(file))
+            return res.send({ status: 'ERROR', message: 'Plugin not found' });
+
+        fs.unlinkSync(file);
 
         res.send({ status: 'OK' });
     });
@@ -247,7 +406,8 @@ async function installSoftware(software, serverID, db) {
     ], { cwd: serverDir });
 
     serverProcess.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
+        if (config.extensiveLogging)
+            console.log(`stdout: ${data}`);
     });
 
     serverProcess.stderr.on('data', (data) => {
@@ -269,7 +429,11 @@ async function installSoftware(software, serverID, db) {
 
 async function downloadSoftware(software) {
     //download software
-    await downloadFile(config.software[software].downloadUrl, `minecraft/software/${software}.jar`);
+    try {
+        await downloadFile(config.software[software].downloadUrl, `minecraft/software/${software}.jar`);
+    } catch (e) {
+        console.error(e);
+    }
 }
 
 async function downloadFile(url, path) {
@@ -281,7 +445,7 @@ async function downloadFile(url, path) {
                 file.close(resolve);  // close() is async, call cb after close completes.
             });
         }).on('error', function (err) { // Handle errors
-            fs.unlink(path); // Delete the file async. (But we don't check the result)
+            fs.unlinkSync(path); // Delete the file
             reject(err.message);
         });
     });
@@ -299,13 +463,89 @@ function prepareDirectorys() {
 }
 
 io.on('connection', function (socket) {
-    console.log('A user connected');
+    socket.on('server_cmd', (data) => {
+        const serverID = data.id;
+        const serverDir = `minecraft/servers/${serverID}`;
+        runningServers.forEach(async (serverProcess, dir) => {
+            if (dir === serverDir) {
+                console.log(`stdin: ${data.command}`);
+                serverProcess.stdin.write(`${data.command}\n`);
+            }
+        });
+    });
 
-    socket.on('disconnect', function () {
-        console.log('A user disconnected');
+    socket.on('search_mods', async (value, version) => {
+        try {
+            const response = await fetch(`https://api.modrinth.com/v2/search?limit=100&index=relevance&facets=[["categories:fabric"],["versions:${version}"],["server_side:required","server_side:optional"]]&query=${value}`);
+            response.json().then((data) => {
+                socket.emit('mods', data);
+            });
+        } catch (e) {
+            if (config.extensiveLogging)
+                console.error(e);
+        }
     });
 });
 
-http.listen(process.env.PORT || 3000, function () {
+http.listen(process.env.PORT || 3000, () => {
     console.log(`listening on *:${process.env.PORT || 3000}`);
 });
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+//Kill all active minecraft servers before leaving
+process.on('beforeExit', () => {
+    runningServers.forEach(async (serverProcess) => {
+        console.log('killing server process');
+        serverProcess.stdin.write('/stop\n');
+        await sleep(5000);
+        serverProcess.kill(0);
+    });
+});
+
+async function getModList(serverDir) {
+    //Create mod directory if needed
+    if (!fs.existsSync(`${serverDir}/mods`))
+        fs.mkdirSync(`${serverDir}/mods`);
+
+    const mods = fs.readdirSync(`${serverDir}/mods`);
+
+    let versionList = [];
+    let modList = [];
+
+    for (let i = 0; i < mods.length; i++) {
+        const mod = mods[i];
+        const modPath = `${serverDir}/mods/${mod}`;
+        const modFile = fs.readFileSync(modPath);
+        const modHash = crypto.createHash('sha1').update(modFile).
+            digest('hex');
+        const versionUrl = `https://api.modrinth.com/v2/version_file/${modHash}?algorithm=sha1`;
+
+        try {
+            const d = await fetch(versionUrl);
+            versionList[versionList.push(await d.json()) - 1].file = mods[i];
+        } catch (e) {
+            // eslint-disable-next-line camelcase
+            modList.push({title: mods[i], icon_url: '/static/icons/favicon.ico', description: 'Could not find mod on modrinth', body: 'This is most probably the case because the mod was loaded manually.', file: mods[i]});
+            if (config.extensiveLogging)
+                console.error(e);
+        }
+    }
+
+    for (let i = 0; i < versionList.length; i++) {
+        try {
+            // eslint-disable-next-line no-await-in-loop
+            const d = await fetch(`https://api.modrinth.com/v2/project/${versionList[i].project_id}`);
+            // eslint-disable-next-line no-await-in-loop
+            modList[modList.push(await d.json()) - 1].file = versionList[i].file;
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    return modList;
+}
---
+++ static/scripts/mods-page.js
@@ -0,0 +1,95 @@
+const socket = io();
+
+socket.on('mods', function (data) {
+    document.getElementById('install-mods').innerHTML = '';
+    for (let i = 0; i < data.hits.length; i++) {
+        let mod = data.hits[i];
+        let modDiv = document.createElement('div');
+        modDiv.className = 'mod';
+        modDiv.innerHTML = `
+        <image class="mod-icon" src="${mod.icon_url}"></image>
+        <div class="mod-info">
+            <h2 class="mod-name">${mod.title}</h2>
+            <div class="mod-author">${mod.author}</div>
+            <div class="mod-description">${mod.description}</div>
+            <div class="mod-buttons">
+                <input type="button" class="btn" onclick="download('${mod.slug}')" value="Install"></input>
+            </div>
+        </div>
+        `;
+        document.getElementById('install-mods').appendChild(modDiv);
+    }
+});
+
+socket.on('mod_list', (data) => {
+    document.getElementById('installed-mods').innerHTML = '';
+    for (let i = 0; i < data.length; i++) {
+        let mod = data[i];
+        let modDiv = document.createElement('div');
+        modDiv.className = 'mod';
+        modDiv.tabIndex = 0;
+        modDiv.innerHTML = `
+        <image class="mod-icon" src="${mod.icon_url}"></image>
+            <div class="mod-info">
+                <h2 class="mod-name">${mod.title}</h2>
+                <div class="mod-description">${mod.description}</div>
+                <div class="mod-body">${mod.body.replaceAll('\n', '<br>')}</div>
+                <div class="mod-buttons">
+                    <input type="button" class="btn btn-danger" onclick="remove('${mod.file}')" value="Remove"></input>
+                </div>
+            </div>
+        `;
+        document.getElementById('installed-mods').appendChild(modDiv);
+    }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const modSearch = document.getElementById('mod-search');
+    const version = document.getElementById('container').dataset.version;
+
+    socket.emit('search_mods', modSearch.value, version);
+
+    modSearch.addEventListener('change', () => {
+        socket.emit('search_mods', modSearch.value, version);
+    });
+
+    modSearch.addEventListener('input', () => {
+        socket.emit('search_mods', modSearch.value, version);
+    });
+});
+
+// eslint-disable-next-line no-unused-vars
+function download(slug) {
+    const request = new XMLHttpRequest();
+    const server = document.getElementById('container').dataset.id;
+
+    request.open('POST', `/api/server/${server}/mods/${slug}`);
+    request.send();
+
+    request.addEventListener('load', function() {
+        const response = JSON.parse(request.responseText);
+        const status = response.status;
+
+        if (status === 'ERROR') {
+            createModal('Error', `${response.message}`);
+        }
+    });
+}
+
+// eslint-disable-next-line no-unused-vars
+function remove(filename) {
+    const request = new XMLHttpRequest();
+    const server = document.getElementById('container').dataset.id;
+
+    request.open('DELETE', `/api/server/${server}/mods/${filename}`);
+    request.send();
+
+    request.addEventListener('load', function() {
+        const response = JSON.parse(request.responseText);
+        const status = response.status;
+
+        if (status === 'ERROR') {
+            createModal('Error', `${response.message}`);
+        }
+    });
+}
